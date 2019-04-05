42db03a5fde00795c7981a3ee294eda6268e0bae
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2019-04-05 08:35:56 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -8,8 +8,8 @@ module.exports = (message) => {
         return;
       }
 
-      const command = message.content.split(' ')[0].slice(client.prefix.length);
-  const params = message.content.split(' ').slice(1);
+      const command = message.content.split(" ")[0].slice(client.prefix.length);
+  const params = message.content.split(" ").slice(1);
   let cmd;
   if (client.commands.has(command)) {
     cmd = client.commands.get(command);
