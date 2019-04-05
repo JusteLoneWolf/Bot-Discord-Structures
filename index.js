ffced288dfc9e95e0b492e89dc5fe4be523b0338
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2019-04-05 08:22:27 +0200

    Update index.js
    Some code Fix

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client({
   disableEveryone: true,
   fetchAllMembers: false,
 })
-const fs = require('fs')
+const fs = require("fs");
 client.prefix = '/' //change your prefix
-require('./utils/eventLoader')(client);//export event
+require("./utils/eventLoader")(client);//export event
     
 client.commands = new Discord.Collection();//create colection
 client.aliases= new Discord.Collection();//create colection
-fs.readdir('./commandes/info', (err, files) => {
+fs.readdir("./commandes/info", (err, files) => {
   files.forEach(f => {
     const props = require(`./commandes/info/${f}`);
     client.commands.set(props.help.name, props);
@@ -18,7 +18,7 @@ fs.readdir('./commandes/info', (err, files) => {
     });
   });
 });
-fs.readdir('./commandes/fun', (err, files) => {       //copy this to each new folder
+fs.readdir("./commandes/fun", (err, files) => {       //copy this to each new folder
   files.forEach(f => {                                //copy this to each new folder
     const props = require(`./commandes/fun/${f}`);    //copy this to each new folder
     client.commands.set(props.help.name, props);      //copy this to each new folder
@@ -27,4 +27,4 @@ fs.readdir('./commandes/fun', (err, files) => {       //copy this to each new fo
     });                                               //copy this to each new folder
   });                                                 //copy this to each new folder
 });                                                   //copy this to each new folder
-client.login(token);//change token bot
+client.login(client.token);//change token bot
