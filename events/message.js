c1b94140239b001da77686f2e396fe5964aa7c1a
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2019-04-05 08:54:36 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,12 +1,12 @@
 module.exports = (message) => {
   if (message.author.bot){
     return;
-  };
+  }
     const client = message.client;
 
       if(!message.content.startsWith(client.prefix)){
         return;
-      };
+      }
 
       const command = message.content.split(" ")[0].slice(client.prefix.length);
   const params = message.content.split(" ").slice(1);
